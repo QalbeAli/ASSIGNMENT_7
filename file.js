632baa31bb2852c21611a90a57e9a9814e6a047f
MAC <mac@Qalb-E-Ali-Mac.local>
2023-08-20 14:08:25 +0500

    first commit

---
+++ file.js
@@ -0,0 +1,42 @@
+"use strict";
+// ------------Question 2 Personal Message---------------//
+// let personName:string = "Qalb e Ali"
+// console.log(`Welcome To The Typescript Class ${personName}`)
+// ------------Question 3 Name Case ---------------//
+// console.log(personName.toLowerCase())
+// console.log(personName.toUpperCase())
+// ------------Question 4 Famous Quote ---------------//
+// let author = "Albert Einstein" 
+// let quote = "A person who never made a mistake never tried anything new.
+// console.log(`${author} Once Said, "${quote}" `)
+// ------------Question 5 Famous Quote ---------------//
+// Done Completed in The Aboove
+// ------------Question 6 Stripping Names---------------//
+// let fName ="    Ali    "
+// console.log(fName.trim())
+// ------------Question 7 & 8 Mathematics   ---------------//
+// let addition:number = 4+4;
+// let substraction:number = 10-2;
+// let multiplication:number = 4*2
+// let divison:number = 64/8;
+// console.log(addition)
+// console.log(substraction)
+// console.log(multiplication)
+// console.log(divison)
+// ------------Question 9 Favorite Numer   ---------------//
+// let favrtNumer:number = 14
+// console.log(`Hey Everyone! ${favrtNumer} is my favorite Number`)
+// ------------Question 10 Comments   ---------------//
+// I am going to write a function which iss simple
+// function normalFunction(condition) {
+//     // code to execute
+// }
+// ------------Question 11 Array Accessing   ---------------//
+// let names:string[] = ["Ali","Habib","Naveed","Hurairah"]
+// console.log(...names)
+// ------------Question 11 Array Greeting   ---------------//
+// let friendsNames:string[] = ["Ali","Habib","Naveed","Hurairah"]
+// let message:string = "Assalm u Alikum From"
+// for(let i=0; i<friendsNames.length; i++){
+    // console.log(`${message} ${names[i]}`)
+// }
